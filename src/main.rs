3e60d1cb7fcae110c7e09ffae64097f5e489c8d1
Jasdeep Singh Dhillon <jdps2867@gmail.com>
2022-06-24 20:15:30 -0700

    Added button to the window

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
 use gtk::prelude::*;
-use gtk::Application;
+use gtk::{Application, ApplicationWindow, Button};
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld1";
 
 fn main() {
+    // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
+    // Connecting to "activate" singal of 'app'
+    app.connect_activate(build_ui);
+
+    // Running the application
     app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Adding button to the window
+    let button = Button::builder()
+        .label("Press me")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connecting button to a click listener
+    button.connect_clicked(move |button| {
+        // Setting the label to "Hello World" when clicked
+        button.set_label("Hello World");
+    });
+
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Show the window
+    window.present();
 }
