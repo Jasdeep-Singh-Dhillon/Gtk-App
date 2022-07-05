9adbd57c98b5412162535553480f0c95e24e690d
Jasdeep Singh Dhillon <jdps2867@gmail.com>
2022-07-05 15:42:02 -0700

    Using GTK Box in Application window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button};
+use std::cell::Cell;
+use std::rc::Rc;
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld1";
+use gtk::prelude::*;
+use gtk::{self, Application, ApplicationWindow, Button, Orientation};
+
+const APP_ID: &str = "org.gtk_rs.GObjectMemoryManagement";
 
 fn main() {
     // Create a new application
@@ -15,26 +18,42 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    // Adding button to the window
-    let button = Button::builder()
-        .label("Press me")
+    // Creating two buttons
+    let button_increase = Button::builder()
+        .label("Increase")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    let button_decrease = Button::builder()
+        .label("Decrease")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
 
-    // Connecting button to a click listener
-    button.connect_clicked(move |button| {
-        // Setting the label to "Hello World" when clicked
-        button.set_label("Hello World");
-    });
+    // A mutable integer for increment and decrement
+    let mut number: Rc<Cell<i32>> = Rc::new(Cell::new(0));
 
-    // Create a window and set the title
+    // Connecting callbacks
+    // When a button is clicked number value will change
+    button_increase.connect_clicked( move |_| number+=1);
+    button_decrease.connect_clicked(|_| number-=1); 
+
+    // Add Buttons to gtk_box
+    let gtk_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
+
+    // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
-        .child(&button)
+        .child(&gtk_box)
         .build();
 
     // Show the window
